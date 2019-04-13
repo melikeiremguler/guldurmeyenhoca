b9a289836b5b01ef9a65d1c653cbfc6eed632b24
Irem Guler <melike.irem.guler@gmail.com>
2019-04-14 00:56:25 +0300

    duzeltme

--- oopProje/Form1.Designer.cs
+++ oopProje/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
+            this.cmbProduct = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbFiltre = new System.Windows.Forms.ComboBox();
+            this.lbPruduct = new System.Windows.Forms.Label();
+            this.lbFilter = new System.Windows.Forms.Label();
+            this.lbProductCount = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBill);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 450);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnBill
+            // 
+            this.btnBill.Location = new System.Drawing.Point(0, 219);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Size = new System.Drawing.Size(127, 23);
+            this.btnBill.TabIndex = 2;
+            this.btnBill.Text = "Bill";
+            this.btnBill.UseVisualStyleBackColor = true;
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.FormattingEnabled = true;
+            this.cmbProduct.Location = new System.Drawing.Point(141, 20);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(97, 21);
+            this.cmbProduct.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbProductCount);
+            this.groupBox2.Controls.Add(this.lbFilter);
+            this.groupBox2.Controls.Add(this.lbPruduct);
+            this.groupBox2.Controls.Add(this.cmbFiltre);
+            this.groupBox2.Controls.Add(this.cmbProduct);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(127, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 41);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // cmbFiltre
+            // 
+            this.cmbFiltre.FormattingEnabled = true;
+            this.cmbFiltre.Location = new System.Drawing.Point(340, 20);
+            this.cmbFiltre.Name = "cmbFiltre";
+            this.cmbFiltre.Size = new System.Drawing.Size(97, 21);
+            this.cmbFiltre.TabIndex = 1;
+            // 
+            // lbPruduct
+            // 
+            this.lbPruduct.AutoSize = true;
+            this.lbPruduct.Location = new System.Drawing.Point(43, 27);
+            this.lbPruduct.Name = "lbPruduct";
+            this.lbPruduct.Size = new System.Drawing.Size(44, 13);
+            this.lbPruduct.TabIndex = 2;
+            this.lbPruduct.Text = "Product";
+            // 
+            // lbFilter
+            // 
+            this.lbFilter.AutoSize = true;
+            this.lbFilter.Location = new System.Drawing.Point(283, 27);
+            this.lbFilter.Name = "lbFilter";
+            this.lbFilter.Size = new System.Drawing.Size(29, 13);
+            this.lbFilter.TabIndex = 3;
+            this.lbFilter.Text = "Filter";
+            // 
+            // lbProductCount
+            // 
+            this.lbProductCount.AutoSize = true;
+            this.lbProductCount.Location = new System.Drawing.Point(608, 27);
+            this.lbProductCount.Name = "lbProductCount";
+            this.lbProductCount.Size = new System.Drawing.Size(13, 13);
+            this.lbProductCount.TabIndex = 4;
+            this.lbProductCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBill;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbProduct;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbFilter;
+        private System.Windows.Forms.Label lbPruduct;
+        private System.Windows.Forms.ComboBox cmbFiltre;
+        private System.Windows.Forms.Label lbProductCount;
     }
 }
 
